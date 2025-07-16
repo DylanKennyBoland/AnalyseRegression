8874830ef83318b01b468dfab2b5fe171910d12b
Dylan Boland <dylan.boland@ucdconnect.ie>
2025-07-16 14:53:01 +0100

    Mostly tidy up: removing code (message strings) that are not needed for this script. I am also reordering things so that the script doesn't do anything unnecessary (e.g., create variables, files etc.) before exiting out early.

--- analyse_regression.py
+++ analyse_regression.py
@@ -17,9 +17,9 @@ import sys
 
 # ==== Some General-info Strings ====
 # Tags - define these here so they can be quickly and easily changed
-errorTag   = """\t***Error: """
-successTag = """\t***Success: """
-infoTag    = """\t***Info: """
+errorTag   = """***Error: """
+successTag = """***Success: """
+infoTag    = """***Info: """
 
 # String to indicate the name of the repository:
 repoName = "mcip"
@@ -48,33 +48,18 @@ It returns:
 A list of all the error signatures (marks or IDs) which occurred
 in the regression and their counts (i.e., their frequency)."""
 noArgsMsg = infoTag + "No optional arguments were supplied."
-
-noSuchFileMsg   = errorTag + """The module file '{}' could not be located - double-check the name or file path."""
-fileReadSuccess = successTag + """The module file was read in successfully."""
-fileReadError   = errorTag + """The module file could not be read in successfully."""
-moduleNameNotIdentified = errorTag + """The module name could not be identified. Please ensure that you have used
-\ta standard or conventional structure. An example is shown below:
-            module serialTX #
-                (
-                parameter INCR = 26'd25770 // amount by which the accumulator is incremented
-                )
-                (
-                input clk,
-                input reset,
-                input [7:0] data,
-                input send,
-                output reg txOut,
-                output busy
-                );
-                // The module description or logic goes below
-"""
-noParamsFound       = infoTag + """No parameters were identified for this module."""
-noInputsIdentified  = errorTag + """No module inputs were identified."""
-noOutputsIdentified = errorTag + """No module outputs were identified."""
+fileReadSuccess = successTag + """The log file at {} was read in successfully."""
 configSuppliedMsg = infoTag + """The following configuration has been supplied on the command line: {}"""
 fastSearchRequestedMsg = infoTag + """A fast search has been requested on the command line."""
+verbosityEnabledMsg = infoTag + """Verbosity has been enabled from the command line."""
 
-# Instantiated module print-out message
+# Error messages:
+noSuchConfigMsg = errorTag + """The config '{}' could not be located - double-check the name."""
+fileReadError   = errorTag + """The log file at {} file could not be read in successfully."""
+wrongDirectoryMsg = errorTag + f"""The script appears to have been called from the wrong directory.
+The {repoName} repository can't be found."""
+
+# End print-out message
 jobDoneMsg = """
 \n\n\t===========================================================================
 \t\tAdditionally, you can see the regression analysis below!
@@ -86,54 +71,60 @@ goodbyeMsg = """
 \t\tSuccess! The analysis report is at {}.
 \t==========================================================================================="""
 
-# Some Header (Delimiter) Strings
-# These allow us to cleary separate input and
-# output ports when instantiating the module
-inputPortsHeader = """// ==== Inputs ===="""
-outputPortsHeader = """// ==== Outputs ====\n\t"""
-paramsHeader = """// ==== Parameters ===="""
-
 # Function to handle the input arguments
 def parsingArguments():
     parser = argparse.ArgumentParser(description = descriptionMsg, formatter_class = RawTextHelpFormatter)
     parser.add_argument('--config', type = str, required = False, help = helpMsg)
     parser.add_argument('--fast_search', action = 'store_true', required = False, help = helpMsg)
+    parser.add_argument('--verbose', '-v', action = 'store_true', required = False, help = helpMsg)
     return parser.parse_args()
 
 if __name__ == "__main__":
+    # ==== Step 1: Get the Path to the Current Work Directory (CWD) ====
+    cwd = os.getcwd()
+
+    # ==== Step 2: Get the Name of the Folder in which the Repository is Cloned ====
+    cwdSplitUp = cwd.split("\\") # Split the path to the current working directory up
+
+    # Check if some segment of the path *is* the name of the repository:
+    pathContainsRepo = repoName in cwdSplitUp
+    # If the repo name is not found, alert the user and exit:
+    if not pathContainsRepo:
+        print(wrongDirectoryMsg)
+        exit()
+
+    # ==== Step 3: Parse the Input Arguments (if there are any) ====
     args = parsingArguments() # parse the input arguments (if there are any)
-    argsSupplied = True
-    configSupplied = False
-    fastSearchRequested = False
     if len(sys.argv) == 1:
         print(noArgsMsg)
-        argsSupplied = False
-    elif args.config is not None:
+
+    verbosityEnabled = False
+    if args.verbose:
+        verbosityEnabled = True
+        print(verbosityEnabledMsg)
+
+    configSupplied = False
+    if args.config is not None:
         configName = args.config
         configSupplied = True
         print(configSuppliedMsg.format(configName))
-        if args.fast_search:
-            fastSearchRequested = True
-            print(fastSearchRequestedMsg)
-    elif args.fast_search:
+
+    fastSearchRequested = False
+    if args.fast_search:
         fastSearchRequested = True
         print(fastSearchRequestedMsg)
-    # ==== Step 1: Get the Path to the Current Work Directory (CWD) ====
-    cwd = os.getcwd()
-    # ==== Step 2: Get the Name of the Folder in which the Repository is Cloned ====
-    cwdSplitUp = cwd.split("\\") # Split the path to the current working directory up
-    # Check if some segment of the path *is* the name of the repository:
-    pathContainsRepo = repoName in cwdSplitUp
+
+    exit()
     indexOfReposFolder = 0
     if (pathContainsRepo):
         cwdSplitUpReversed = list(reversed(cwdSplitUp))
         indexOfReposFolder = cwdSplitUpReversed.index(repoName)
         if indexOfReposFolder != (len(cwdSplitUpReversed) - 1):
             indexOfReposFolder = indexOfReposFolder + 1
-    scratchFolderName = cwdSplitUpReversed[indexOfReposFolder]
+    reposFolderName = cwdSplitUpReversed[indexOfReposFolder]
     userName = os.environ.get('USERNAME')
-    scratchDirBaseName = f"/lan/{userName}/{scratchFolderName}/"
+    scratchDirBaseName = f"/lan/{userName}/{reposFolderName}/"
     print(cwdSplitUp)
-    print(scratchFolderName)
+    print(reposFolderName)
     print(scratchDirBaseName)
     exit()
